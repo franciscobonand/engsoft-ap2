bcba1c9527d20f7888eefaa2cd2f0b83e5efa0bc
Francisco Bonome Andrade <bonand04@gmail.com>
2021-03-01 20:44:29 +0000

    Commit 11

--- Movie.java
+++ Movie.java
@@ -5,19 +5,31 @@ public class Movie {
   public static final int  NEW_RELEASE = 1;
 
   private String _title;
-  private int _priceCode;
+  private Price _price;
 
-  public Movie(String title, int priceCode) {
-      _title = title;
-      _priceCode = priceCode;
+  public Movie(String name, int priceCode) {
+    _title = name;
+    setPriceCode(priceCode);
   }
-
+  
   public int getPriceCode() {
-      return _priceCode;
+    return _price.getPriceCode();
   }
-
+  
   public void setPriceCode(int arg) {
-      _priceCode = arg;
+    switch (arg) {
+        case REGULAR:
+          _price = new RegularPrice();
+          break;
+        case CHILDRENS:
+          _price = new ChildrensPrice();
+          break;
+        case NEW_RELEASE:
+          _price = new NewReleasePrice();
+          break;
+        default:
+          throw new IllegalArgumentException("Incorrect Price Code");
+    }
   }
 
   public String getTitle (){
@@ -27,7 +39,7 @@ public class Movie {
   public double getCharge(int daysRented) {
     double thisAmountTemp = 0;
     //determine amounts for each line
-    switch (_priceCode) {
+    switch (getPriceCode()) {
       case Movie.REGULAR:
         thisAmountTemp += 2;
         if (daysRented > 2)
@@ -46,7 +58,7 @@ public class Movie {
   }
 
   public int getFrequentRenterPoints(int daysRented) {
-    switch (_priceCode) {
+    switch (getPriceCode()) {
       case Movie.NEW_RELEASE:
         return 2;
       default :

8c5906bff30d81cba7ffb2219996050ee20fb9f7
Francisco Bonome Andrade <bonand04@gmail.com>
2021-03-01 20:46:00 +0000

    Commit 12

--- Movie.java
+++ Movie.java
@@ -37,25 +37,8 @@ public class Movie {
   }
 
   public double getCharge(int daysRented) {
-    double thisAmountTemp = 0;
-    //determine amounts for each line
-    switch (getPriceCode()) {
-      case Movie.REGULAR:
-        thisAmountTemp += 2;
-        if (daysRented > 2)
-            thisAmountTemp += (daysRented - 2) * 1.5;
-        break;
-      case Movie.NEW_RELEASE:
-        thisAmountTemp += daysRented * 3;
-        break;
-      case Movie.CHILDRENS:
-        thisAmountTemp += 1.5;
-        if (daysRented > 3)
-            thisAmountTemp += (daysRented - 3) * 1.5;
-          break;
-    }
-    return thisAmountTemp;
-  }
+         return _price.getCharge(daysRented);
+   }
 
   public int getFrequentRenterPoints(int daysRented) {
     switch (getPriceCode()) {
--- Price.java
+++ Price.java
@@ -1,3 +1,24 @@
 public abstract class Price {
-   public abstract int getPriceCode();
+  public abstract int getPriceCode();
+
+  public double getCharge(int daysRented) {
+    double thisAmountTemp = 0;
+    //determine amounts for each line
+    switch (getPriceCode()) {
+      case Movie.REGULAR:
+        thisAmountTemp += 2;
+        if (daysRented > 2)
+            thisAmountTemp += (daysRented - 2) * 1.5;
+        break;
+      case Movie.NEW_RELEASE:
+        thisAmountTemp += daysRented * 3;
+        break;
+      case Movie.CHILDRENS:
+        thisAmountTemp += 1.5;
+        if (daysRented > 3)
+            thisAmountTemp += (daysRented - 3) * 1.5;
+          break;
+    }
+    return thisAmountTemp;
+  }
 }
